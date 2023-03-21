a9433b5290a6bec5ebcd400a16468e2314718545
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2023-03-21 16:20:00 -0400

    2nd init

---
+++ root/Collisions-Murray/Assets/LevelUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelUp : MonoBehaviour {
+    private void Start()
+    {
+        
+    }
+    public void OnClick()
+    {
+        if(SceneManager.GetActiveScene().name == "menuScene")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("menuScene");
+        }
+        
+    }
+}
